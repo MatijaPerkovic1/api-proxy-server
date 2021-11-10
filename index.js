15681095ef95e9430d8b7ef34ce54dfc51500906
unknown <mperkovic@protonmail.com>
2021-11-10 12:19:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api', require('./routes'));
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
