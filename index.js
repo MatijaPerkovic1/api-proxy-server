835a95cb3621a8ed6a3f3a9035002d7266ce45b5
unknown <mperkovic@protonmail.com>
2021-11-10 16:44:07 +0100

    add rate limiter and caching

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 min
+    max: 1000
+});
+
+app.use(limiter);
+app.set('trust proxy', 1);
 
 app.use('/api', require('./routes'));
 
